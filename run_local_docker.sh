0d9f90e5e3d067fc5f15747426be214d42310b7a
richelbilderbeek <richel@richelbilderbeek.nl>
2024-03-07 07:50:43 +0100

    Progress

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -4,8 +4,12 @@
 rm -f metontiime2.conf
 wget https://raw.githubusercontent.com/MaestSi/MetONTIIME/master/metontiime2.conf
 
-# Change the number of processors to 4
+# Change the number of processors from 6 to 4
 sed -i 's/ ? 6 : 1 }/ ? 4 : 1 }/' metontiime2.conf
+
+# Change the memory use from 10 to 7 GB
+sed -i 's/ 10.GB / 7.GB /' metontiime2.conf
+
 
 # Run locally
 nextflow -c metontiime2.conf run metontiime2.nf \
