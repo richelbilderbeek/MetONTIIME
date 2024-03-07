2cddb54fb80bb39959e6b9664365766d73412eca
richelbilderbeek <richel@richelbilderbeek.nl>
2024-03-07 08:21:10 +0100

    Add other options

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -10,12 +10,22 @@ sed -i 's/ ? 6 : 1 }/ ? 4 : 1 }/' metontiime2.conf
 # Change the memory use from 10 to 7 GB
 sed -i 's/ 10.GB / 7.GB /' metontiime2.conf
 
+work_dir="${PWD}/work_local_docker"
+echo "work_dir: ${work_dir}"
+
+results_dir="${PWD}/results_local_docker"
+echo "results_dir: ${results_dir}"
+
+db_sequence_fasta_filename="${PWD}/sequence.fasta"
+echo "db_sequence_fasta_filename: ${db_sequence_fasta_filename}"
+
+sample_metadata_tsv_filename="${PWD}/sample-metadata.tsv"
+echo "sample_metadata_tsv_filename: ${sample_metadata_tsv_filename}"
 
 # Run locally
 nextflow -c metontiime2.conf run metontiime2.nf \
-  --workDir=${PWD} \
-  --resultsDir="${PWD}/results_local_docker" \
+  --workDir="${work_dir}" \
+  --resultsDir="${results_dir}" \
+  --dbSequencesFasta="${db_sequence_fasta_filename}" \
+  --sampleMetadata="${sample_metadata_tsv_filename}" \
   -profile docker 
-
-
-
