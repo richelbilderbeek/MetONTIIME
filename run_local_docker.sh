957f330ae502ff070410860b82813d7d4ada3726
richelbilderbeek <richel@richelbilderbeek.nl>
2024-03-07 07:44:35 +0100

    Need more memory

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-wget -O https://raw.githubusercontent.com/MaestSi/MetONTIIME/master/metontiime2.conf
+
+# Download a fresh config file
+rm -f metontiime2.conf
+wget https://raw.githubusercontent.com/MaestSi/MetONTIIME/master/metontiime2.conf
+
+# Change the number of processors to 4
 sed -i 's/ ? 6 : 1 }/ ? 4 : 1 }/' metontiime2.conf
 
+# Run locally
 nextflow -c metontiime2.conf run metontiime2.nf \
   --workDir=${PWD} \
   --resultsDir="${PWD}/results_local_docker" \
